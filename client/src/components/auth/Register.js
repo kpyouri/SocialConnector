0e067333ff2a9be8de0c171c666c906ff906eaed
Esther <esther.youri.shin@gmail.com>
2018-10-21 00:28:51 -0700

    added in axios

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -13,8 +13,8 @@ class Register extends Component {
             errors: {}
         }
 
-    //this.onChange = this.onChange.bind(this);
-    // this.onSubmit = this.onSubmit.bind(this);
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
     }
 
 
@@ -23,6 +23,20 @@ class Register extends Component {
         this.setState({[e.target.name]: e.target.value});
     }
 
+    onSubmit(e){
+        e.preventDefault();
+
+        const newUser = {
+            name: this.state.name,
+            email: this.state.email,
+            password: this.state.password,
+            password2: this.state.password2
+        };
+
+        axios.post('/api/users/register', newUser)
+        .then(res => console.log(res.data))
+        .catch(err => console.log(err.response.data));
+    }
 
   render() {
     return (
@@ -36,25 +50,25 @@ class Register extends Component {
             <div className="form-group">
               <input type="text" className="form-control form-control-lg" placeholder="Name" name="name"
               value={this.state.name} 
-              onChange={this.onChange.bind(this)} 
+              onChange={this.onChange} 
                />
                
             </div>
             <div className="form-group">
               <input type="email" className="form-control form-control-lg" placeholder="Email Address" name="email"
               value={this.state.email} 
-              onChange={this.onChange.bind(this)}  />
+              onChange={this.onChange}  />
               <small className="form-text text-muted">This site uses Gravatar so if you want a profile image, use a Gravatar email</small>
             </div>
             <div className="form-group">
               <input type="password" className="form-control form-control-lg" placeholder="Password" name="password" 
               value={this.state.password}
-              onChange={this.onChange.bind(this)}  />
+              onChange={this.onChange}  />
             </div>
             <div className="form-group">
               <input type="password" className="form-control form-control-lg" placeholder="Confirm Password" name="password2"
               value={this.state.password2}
-              onChange={this.onChange.bind(this)}   />
+              onChange={this.onChange}   />
             </div>
             <input type="submit" className="btn btn-info btn-block mt-4" />
           </form>
